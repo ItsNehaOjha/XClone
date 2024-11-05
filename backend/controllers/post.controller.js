e01ede4b4740dd42fc37417623702d2bde6f8f8a
Neha <ojhaneha0028@gmail.com>
2024-11-05 20:19:19 +0530

    3-like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -103,7 +103,8 @@ export const likeUnlikePost = async (req,res)=>{
             await Post.updateOne({_id:postId},{$pull:{likes:userId}})
             await User.updateOne({_id:userId},{$pull:{likedPosts:postId}})
             
-            res.status(200).json({message:"Post unlike succesfully"});
+            const updatedLikes = post.likes.filter ((id) => id.toString() !== userId.toString())
+            res.status(200).json(updatedLikes);
         }else{
             //like
             post.likes.push(userId);
@@ -117,7 +118,7 @@ export const likeUnlikePost = async (req,res)=>{
             })
             await notification.save();
 
-            res.status(200).json({message:"Post liked successfully"});
+            res.status(200).json(post.likes);
         }
 
     }catch(error){
